78c09be9f05d59e9b087829ba2d31a1d9abfbb8e
markgreenburg <marek.greenburg@gmail.com>
2016-12-13 11:50:04 -0600

    Breaking down into smaller methods
    Adding more attributes and methods for each class, and breaking down
    game logic into smaller chunks.

--- catch_the_monster_old.py
+++ catch_the_monster_old.py
@@ -30,9 +30,6 @@ class Hero(object):
         self.width = 32
         self.height = 32
 
-    def render(self, screen):
-        screen.blit(self.img, (self.pos_x, self.pos_y))
-
     def move(self):
         self.pos_x += self.spd_x
         self.pos_y += self.spd_y
@@ -175,7 +172,7 @@ def main():
     win_tone = pygame.mixer.Sound("sounds/win.wav")
     win_tone_played = 0
     lose_tone = pygame.mixer.Sound("sounds/lose.wav")
-    lose_tone_played = False
+    lose_tone_played = 0
     pygame.mixer.init()
     # create screen
     screen = pygame.display.set_mode((width, height))
@@ -191,63 +188,102 @@ def main():
     hero_1 = Hero(256, 220, 'images/hero.png')
     # game loop
     stop_game = False
+    collision = False
+    win = True
     game_over = False
     while not stop_game:
-        # EVENT HANDLING CODE
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN: #pylint: disable=E1101
-                if event.key == pygame.K_DOWN:
-                    hero_1.spd_x = 0
-                    hero_1.spd_y = 2
-                elif event.key == pygame.K_UP:
-                    hero_1.spd_x = 0
-                    hero_1.spd_y = -2
-                elif event.key == pygame.K_RIGHT:
-                    hero_1.spd_x = 2
-                    hero_1.spd_y = 0
-                elif event.key == pygame.K_LEFT:
-                    hero_1.spd_x = -2
-                    hero_1.spd_y = 0
-                elif event.key == pygame.K_RETURN:
-                    game_over = False
-                    win_tone_played = 0
-                    lose_tone_played = 0
-                    background_music.play()
-                    monster_1.pos_x = random.randint(0, width)
-                    monster_1.pos_y = random.randint(0, height)
-                    goblin_1.pos_x = random.randint(0, width)
-                    goblin_1.pos_y = random.randint(0, height)
-            if event.type == pygame.QUIT: #pylint: disable=E1101
-                # if they closed the window, set stop_game to True
-                # to exit the main loop
-                stop_game = True
-
-        # UPDATE GAME STATE
-        if detect_collision(hero_1, monster_1):
-            game_over = True
-            background_music.stop()
-            if not win_tone_played:
-                win_tone.play()
-                win_tone_played = True
+        # RESET GAME IF COLLISION DETECTED
+        if collision:
+            # stop moving players
             hero_1.spd_x, hero_1.spd_y = 0, 0
             monster_1.spd_x, monster_1.spd_y = 0, 0
             goblin_1.spd_x, goblin_1.spd_y = 0, 0
-        else:
-            hero_1.update_pos(width, height)
-            monster_1.update_pos(width, height)
-            goblin_1.update_pos(width, height)
-        # CUSTOM DISPLAY CODE
-        screen.blit(bkgrnd_img, (0, 0))
-        if not game_over:
-            monster_1.render(screen)
-            goblin_1.render(screen)
-        else:
+            # create message to ask if want to play again
             font = pygame.font.Font(None, 25)
             text = font.render('Hit Enter to play again', True, (0, 0, 0))
+            # draw items onto screen
+            # Play win tone...just once
+            if win_tone_played == 0 and win:
+                # Stop background music, play lose music. Set lose counter to
+                # one so that music doesn't loop
+                background_music.stop()
+                win_tone.play()
+                win_tone_played = 1
+                # Draw background and hero image only
+                screen.blit(bkgrnd_img, (0, 0))
+                screen.blit(hero_1.img, (hero_1.pos_x, hero_1.pos_y))
+            elif lose_tone_played == 0 and not win:
+                # Stop background music, play lose music. Set lose counter to
+                # one so that music doesn't loop
+                background_music.stop()
+                lose_tone.play()
+                lose_tone_played = 1
+                # Draw background, goblin, and monster images only
+                screen.blit(bkgrnd_img, (0, 0))
+                screen.blit(monster_1.img, (monster_1.pos_x, monster_1.pos_y))
+                screen.blit(goblin_1.img, (goblin_1.pos_x, goblin_1.pos_y))
             screen.blit(text, (170, 220))
-        hero_1.render(screen)
-        pygame.display.update()
-        # tick the clock to enforce a max framerate
+            pygame.display.update()
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:
+                        if event.key == pygame.K_DOWN:
+                            hero_1.spd_x = 0
+                            hero_1.spd_y = +2
+                        elif event.key == pygame.K_UP:
+                            hero_1.spd_x = 0
+                            hero_1.spd_y = -2
+                        elif event.key == pygame.K_RIGHT:
+                            hero_1.spd_x = 2
+                            hero_1.spd_y = 0
+                        elif event.key == pygame.K_LEFT:
+                            hero_1.spd_x = -2
+                            hero_1.spd_y = 0
+                        elif event.key == pygame.K_RETURN:
+                            game_over = False
+                            win_tone_played = 0
+                            lose_tone_played = 0
+                            background_music.play()
+                            monster_1.pos_x = random.randint(0, width)
+                            monster_1.pos_y = random.randint(0, height)
+                            goblin_1.pos_x = random.randint(0, width)
+                            goblin_1.pos_y = random.randint(0, height)
+                if event.type == pygame.QUIT: #pylint: disable=E1101
+                    stop_game = True
+        # PLAY GAME UNTIL COLLISION OR QUIT
+        else:
+            # EVENT HANDLING CODE
+            # Reset win tone
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN: #pylint: disable=E1101
+                    hero_1.register_keypress(event.key)
+                if event.type == pygame.QUIT: #pylint: disable=E1101
+                    # if they closed the window, set stop_game to True
+                    # to exit the main loop
+                    stop_game = True
+            # UPDATE GAME STATE
+            hero_1.render(screen)
+            if not game_over:
+                monster_1.render(screen)
+                hero_1.update_pos(width, height)
+                monster_1.update_pos(width, height)
+                goblin_1.update_pos(width, height)
+            if detect_collision(hero_1, monster_1):
+                game_over = True
+
+
+            # CUSTOM DISPLAY CODE
+
+            else:
+                background_music.stop()
+                win_tone.play()
+                win_tone_played = 1
+            screen.blit(bkgrnd_img, (0, 0))
+            screen.blit(hero_1.img, (hero_1.pos_x, hero_1.pos_y))
+            screen.blit(goblin_1.img, (goblin_1.pos_x, goblin_1.pos_y))
+            pygame.display.update()
+            # tick the clock to enforce a max framerate
         clock.tick(60)
 
     # quit pygame properly to clean up resources
