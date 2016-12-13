21c8cc29542e2cdb297e9273ac0508462e16a24f
markgreenburg <marek.greenburg@gmail.com>
2016-12-13 12:37:23 -0600

    Working again
    Has been rebuilt to previous functionality with more modular
    class-based design. Still have several significant updates to improve
    code reusability, inheritance, and modularity.

--- catch_the_monster.py
+++ catch_the_monster.py
@@ -54,13 +54,6 @@ class Hero(object):
         Hero.move(self)
         Hero.change_direction(self, width, height)
 
-    def register_keypress(self, event_key):
-        '''
-        Event handler for Hero character. Changes movement
-        direction according to keypress.
-        '''
-
-
 class Monster(object):
     '''
     Defines a monster character, its starting positions, and legal moves
@@ -139,63 +132,131 @@ class Monster(object):
         if time.time() - self.last_dir_change >= 2:
             Monster.change_direction(self)
 
+class Goblin(object):
+    '''
+    Defines a goblin character, its starting positions, and legal moves
+    '''
+
+    def __init__(self, pos_x, pos_y, spd_x, spd_y, img):
+        '''
+        Initialize a monster with position, speed, and an image
+        '''
+        self.img = pygame.image.load(img).convert_alpha()
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.spd_x = spd_x
+        self.spd_y = spd_y
+        self.last_dir_change = time.time()
+        self.width = 32
+        self.height = 32
+
+    def render(self, screen):
+        screen.blit(self.img, (self.pos_x, self.pos_y))
+
+    # def setInitialPosition(self, width, height, hero):
+    #     min_x = hero_1.pos_x - SAFEBUFFER
+    #     min_y = hero_1.pos_y - SAFEBUFFER
+    #     max_x = hero_1.pos_x + SAFEBUFFER
+    #     max_y = hero_1.pos_y + SAFEBUFFER
+    #
+    #     safe_x = randint(0, width)
+    #     safe_y = randint(0, height)
+    #
+    #     while safe_x >= min_x and safe_x <= max_x and safe_y >= min_y and safe_y <= max_y:
+    #         return True
+
+    def move(self):
+        self.pos_x += self.spd_x
+        self.pos_y += self.spd_y
+
+    def check_bounds(self, width, height):
+        '''
+        Move monster to other side of screen if he goes offscreen
+        '''
+        if self.pos_x > width:
+            self.pos_x = 0
+        if self.pos_x < 0:
+            self.pos_x = width
+        if self.pos_y > height:
+            self.pos_y = 0
+        if self.pos_y < 0:
+            self.pos_y = height
+
+    def change_direction(self):
+        new_direction = random.randint(1, 4)
+        self.spd_x = 3
+        self.spd_y = 3
+        if new_direction == 1:
+            self.spd_x = 0
+            self.spd_y = -1 * self.spd_y
+        if new_direction == 2:
+            self.spd_y = 0
+        if new_direction == 3:
+            self.spd_x = 0
+        if new_direction == 4:
+            self.spd_x = -1 * self.spd_x
+            self.spd_y = 0
+        self.last_dir_change = time.time()
+
+    def update_pos(self, width, height):
+        '''
+        Updates the position of the monster. Returns the current
+        timer to ensure that direction is only updated every 2
+        seconds
+        '''
+        Goblin.move(self)
+        Goblin.check_bounds(self, width, height)
+        if time.time() - self.last_dir_change >= 2:
+            Goblin.change_direction(self)
+
 def detect_collision(character_1, character_2):
     '''
     Determines if a collision has occured between any two characterss. Returns collision as True or returns nothing.
     '''
     return (character_1.pos_x + character_1.width > character_2.pos_x) and \
-    (character_2.pos_x + character_2.width > character_1.pos_x) and \
-    (character_1.pos_y + character_1.height > character_2.pos_y) and \
-    (character_2.pos_y + character_2.height > character_1.pos_y)
-
-# def collision_trumping(a_type, b_type):
-#     '''
-#     Evaluates whether a collision resulted in a win or loss. Returns True if
-#     player won or False if player lost.
-#     '''
-#     if (a_type == 'hero' or b_type == 'hero') and (a_type == 'monster' or b_type == 'monster'):
-#         return True
-#     else:
-#         return False
+    (character_2.pos_x + character_2.width > character_1.pos_x) and (character_1.pos_y + character_1.height > character_2.pos_y)and (character_2.pos_y + character_2.height > character_1.pos_y)
 
 def main():
     '''
     Main function plays the game until the user chooses to quit
     by closing the window.
     '''
-    # DEFINE CONSTANTS
+    # ----DEFINE CONSTANTS----
     # declare the size of the canvas
     width = 512
     height = 480
     # initialize the pygame framework
     pygame.init() #pylint: disable=E1101
-    # load sounds
+    # load music
+    pygame.mixer.init()
     background_music = pygame.mixer.Sound("sounds/music.wav")
     background_music.play()
     win_tone = pygame.mixer.Sound("sounds/win.wav")
-    win_tone_played = 0
     lose_tone = pygame.mixer.Sound("sounds/lose.wav")
+    win_tone_played = False
     lose_tone_played = False
-    pygame.mixer.init()
-    # create screen
+    # create screen & set caption
     screen = pygame.display.set_mode((width, height))
-    # set window caption
     pygame.display.set_caption('Catch The Monster')
     # create a clock to enforce max framerates
     clock = pygame.time.Clock()
-    # INITIALIZE GAME
+    # ----INITIALIZE GAME----
     bkgrnd_img = \
     pygame.image.load('images/background.png').convert_alpha()
+    # draw characters
     monster_1 = Monster(30, 30, 0, 3, 'images/monster.png')
     goblin_1 = Monster(30, 30, 0, 2, 'images/goblin.png')
     hero_1 = Hero(256, 220, 'images/hero.png')
     # game loop
     stop_game = False
-    game_over = False
+    game_won = False
+    game_lost = False
+    level = 0
     while not stop_game:
-        # EVENT HANDLING CODE
+        # ----EVENT HANDLING CODE----
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN: #pylint: disable=E1101
+                # Move hero according to arrow key pressed
                 if event.key == pygame.K_DOWN:
                     hero_1.spd_x = 0
                     hero_1.spd_y = 2
@@ -208,44 +269,63 @@ def main():
                 elif event.key == pygame.K_LEFT:
                     hero_1.spd_x = -2
                     hero_1.spd_y = 0
+                # Reset game state on press of return key
                 elif event.key == pygame.K_RETURN:
-                    game_over = False
-                    win_tone_played = 0
-                    lose_tone_played = 0
+                    game_won = False
+                    game_lost = False
+                    win_tone_played = False
+                    lose_tone_played = False
                     background_music.play()
                     monster_1.pos_x = random.randint(0, width)
                     monster_1.pos_y = random.randint(0, height)
                     goblin_1.pos_x = random.randint(0, width)
                     goblin_1.pos_y = random.randint(0, height)
+            # If user closes window, quite game
             if event.type == pygame.QUIT: #pylint: disable=E1101
                 # if they closed the window, set stop_game to True
                 # to exit the main loop
                 stop_game = True
-
-        # UPDATE GAME STATE
+        # ----UPDATE GAME STATE----
+        # detect collision between hero and monster
         if detect_collision(hero_1, monster_1):
-            game_over = True
-            background_music.stop()
-            if not win_tone_played:
-                win_tone.play()
-                win_tone_played = True
+            game_won = True
             hero_1.spd_x, hero_1.spd_y = 0, 0
             monster_1.spd_x, monster_1.spd_y = 0, 0
             goblin_1.spd_x, goblin_1.spd_y = 0, 0
+            level += 1
+        # detect collision between hero and goblins
+        elif detect_collision(hero_1, goblin_1):
+            game_lost = True
+            level = 1
+        # if no collisions, update positions as normal
         else:
             hero_1.update_pos(width, height)
             monster_1.update_pos(width, height)
             goblin_1.update_pos(width, height)
-        # CUSTOM DISPLAY CODE
+        # ----CUSTOM DISPLAY CODE----
+        # Always draw background first
         screen.blit(bkgrnd_img, (0, 0))
-        if not game_over:
-            monster_1.render(screen)
-            goblin_1.render(screen)
-        else:
+        # If game over, swap music and display text hint to user
+        if game_won or game_lost:
+            background_music.stop()
+            if game_won:
+                if not win_tone_played:
+                    win_tone.play()
+                    win_tone_played = True
+            if game_lost:
+                if not lose_tone_played:
+                    lose_tone.play()
+                    lose_tone_played = True
             font = pygame.font.Font(None, 25)
             text = font.render('Hit Enter to play again', True, (0, 0, 0))
             screen.blit(text, (170, 220))
-        hero_1.render(screen)
+        # draw hero until game is lost
+        if not game_lost:
+            hero_1.render(screen)
+        # Draw monsters / goblins until game is won
+        if not game_won:
+            monster_1.render(screen)
+            goblin_1.render(screen)
         pygame.display.update()
         # tick the clock to enforce a max framerate
         clock.tick(60)
